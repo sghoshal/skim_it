018dd1d3c9d9d071c88046bbe5d1cff58e46223f
Soumyadeep Ghoshal <soumyadeep.ghoshal@gmail.com>
2014-02-16 07:06:22 -0500

    New file upload with time stamp

--- src/com/dropbox/api/samples/FinalResult.java
+++ src/com/dropbox/api/samples/FinalResult.java
@@ -1,13 +1,15 @@
 package com.dropbox.api.samples;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import android.app.Activity;
-import android.graphics.SumPathEffect;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +21,10 @@ import com.dropbox.client2.DropboxAPI.UploadRequest;
 import com.dropbox.client2.android.AndroidAuthSession;
 import com.dropbox.client2.session.AppKeyPair;
 import com.dropbox.client2.session.Session.AccessType;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 public class FinalResult extends Activity {
 
@@ -32,6 +38,7 @@ public class FinalResult extends Activity {
 
 	// In the class declaration section:
 	private DropboxAPI<AndroidAuthSession> mDBApi;
+	
 
 	/** Called when the activity is first created. */
 	@Override
@@ -43,9 +50,9 @@ public class FinalResult extends Activity {
 		summary = getIntent().getExtras().getString("summary").toString();
 
 		TextView summaryView = (TextView) findViewById(R.id.summaryText);
-		
+
 		summaryView.setText(summary);
-		
+
 		AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
 		AndroidAuthSession session = new AndroidAuthSession(appKeys, ACCESS_TYPE);
 		mDBApi = new DropboxAPI<AndroidAuthSession>(session);
@@ -73,11 +80,41 @@ public class FinalResult extends Activity {
 				BufferedWriter bw = new BufferedWriter(new FileWriter(file));
 				bw.write(summary);
 				bw.close();
-				String path = "sampleStarted/sample1.txt";
+				
+				SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy_HH-mm-ss");
+				Date today = Calendar.getInstance().getTime();
+				String repertDate = df.format(today);
+				
+				String path = "Summary/summary_" + repertDate + ".pdf";
 				System.out.println(getApplicationContext().getFilesDir());
-				//File pdffile = new File(getApplicationContext().getFilesDir(), "sample1.pdf");
 
-				UploadPicture up = new UploadPicture(getApplicationContext(), mDBApi, path, file);
+
+				File pdffile = new File(getApplicationContext().getFilesDir(), 
+						"summary_" + repertDate + ".pdf");
+				System.out.println("PDF: " + pdffile.getAbsolutePath().toString());
+
+				try {
+					Document document = new Document();
+					PdfWriter.getInstance(document, new FileOutputStream(pdffile));
+					document.open();
+					//addMetaData(document);
+					//addTitlePage(document);
+					//addContent(document);
+					Paragraph preface = new Paragraph();
+					Font catFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
+							Font.BOLD);
+
+					//addEmptyLine(preface, 1);
+					preface.add(new Paragraph(summary, catFont));
+					document.add(preface);
+
+					document.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				UploadPicture up = new UploadPicture(getApplicationContext(), mDBApi, path, 
+						pdffile);
 				up.execute();
 
 
